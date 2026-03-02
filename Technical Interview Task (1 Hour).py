fef00f2c9daa3367160bc0f98524175b6c9fe2b3
Hemal Shah <hemal.shah2004@gmail.com>
2026-03-02 14:19:45 +0530

    backend code

--- Technical Interview Task (1 Hour).py
+++ Technical Interview Task (1 Hour).py
@@ -1,28 +1,28 @@
-Technical Interview Task (1 Hour)
-NOTE:DON’T USE CHATGPT
-1. Online Examination System
-The system must include the following required features:
-1. Build a complete web application using Python + Django or Flask
-2. Create an Online Examination System with CRUD operations + exam logic
-3. Admin Module must include:
-o Add Subjects
-o Add Questions
-o Edit/Delete Questions
-4. User Module (Student Panel) must allow:
-o Select Subject
-o Attempt Quiz
-o Submit Answers
-o View Score Immediately
-5. Use PostgreSQL / MySQL / SQLite for backend database storage
-2. Expected Submission Deliverables
-To consider the task complete, the candidate must submit:
-1. GitHub Repository Link with clean and readable code structure
-2. README.md including:
-o Setup commands
-o Database migration steps
-o Run instructions
-o Screenshots of the working application
-3. UI must be created using Bootstrap / HTML / CSS OR application can be tested via
-Postman
-4. A functional scoreboard should be displayed after quiz completion
-This ensures evaluation of code quality, documentation skills, and working application usability.
+# Technical Interview Task (1 Hour)
+# NOTE:DON’T USE CHATGPT
+# 1. Online Examination System
+# The system must include the following required features:
+# 1. Build a complete web application using Python + Django or Flask
+# 2. Create an Online Examination System with CRUD operations + exam logic
+# 3. Admin Module must include:
+# # o Add Subjects
+# o Add Questions
+# o Edit/Delete Questions
+# 4. User Module (Student Panel) must allow:
+# o Select Subject
+# o Attempt Quiz
+# o Submit Answers
+# o View Score Immediately
+# 5. Use PostgreSQL / MySQL / SQLite for backend database storage
+# 2. Expected Submission Deliverables
+# To consider the task complete, the candidate must submit:
+# 1. GitHub Repository Link with clean and readable code structure
+# 2. README.md including:
+# o Setup commands
+# o Database migration steps
+# o Run instructions
+# o Screenshots of the working application
+# 3. UI must be created using Bootstrap / HTML / CSS OR application can be tested via
+# Postman
+# 4. A functional scoreboard should be displayed after quiz completion
+# This ensures evaluation of code quality, documentation skills, and working application usability.
--- main.py
+++ main.py
@@ -17,3 +17,62 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# admin module
+@app.post("/admin", response_class=HTMLResponse)
+def admin(request: Request , db: Session = Depends(get_db) , subject: str = db.query(models.Subject).all()):
+    question = db.query(models.Question).all()
+    return templates.TemplateResponse("admin.html" , {"request": request , "subject": subject , "question": question})
+@app.post("admin/subject")
+def add_subject(name: str = FORM(...), db: Session = Depends(get_db)):
+    new_subject = models.Subject(name=name)
+    db.add(new_subject)
+    db.commit()
+    return RedirectResponse(url="/admin", status_code=303)
+
+@app.post("/admin/question")
+def add_question(subject_id: int = Form(...),text: str = Form(...), option_a: str = Form(...),
+                  option_b: str = Form(...), 
+                  option_c: str = Form(...), 
+                  option_d: str = Form(...),
+                    correct_option: str = Form(...), 
+                    db: Session = Depends(get_db)):
+    q= models.Question(subject_id=subject_id, text=text, option_a=option_a, option_b=option_b, option_c=option_c, option_d=option_d, correct_option=correct_option)
+    db.add(q)
+    db.commit()
+    return RedirectResponse(url="/admin", status_code=303)
+
+@app.post("admin/question/{q_id}/delete")
+def delete_question(q_id: int, db: Session =Depends(get_db)):
+    question = db.query(models.Question).filter(models.Question.id == q_id).first()
+    if question:
+        db.delete(question)
+        db.commit()
+    return RedirectResponse(url="/admin", status_code=303)
+
+# user module
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request , db: Session = Depends(get_db) , subject: str = db.query(models.Subject).all()):
+    return templates.TemplateResponse("index.html" , {"request": request , "subject": subject})
+
+@app.get("/quiz/{subject_id}", response_class=HTMLResponse)
+def take_quiz(request: Request, subject_id: int, db: Session = Depends(get_db)):
+    subject = db.query(models.Subject).filter(models.Subject.id == subject_id).first()
+    questions = db.query(models.Question).filter(models.Question.subject_id == subject_id).all()
+    return templates.TemplateResponse("quiz.html", {"request": request, "subject": subject, "questions": questions})
+
+@app.post("/submit_quiz/{subject_id}")
+def submit_quiz(request: Request, subject_id: int, db: Session = Depends(get_db)):
+    form_data = await request.form()
+    questions = db.query(models.Question).filter(models.Question.subject_id == subject_id).all()
+    score = 0
+    total = len(questions)
+    for q in questions:
+        user_answer = form_data.get(f"question_{q.id}")
+        if user_answer == q.correct_option:
+            score += 1
+
+        subject = db.query(models.Subbject).filter(models.Subject.id == subject_id).first()
+    return templates.TemplateResponse("result.html", {
+        "request": request, "score": score, "total": total, "subject": subject
+    })
